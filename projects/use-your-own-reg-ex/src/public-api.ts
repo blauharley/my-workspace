f2eb60e8dd74663a322a5c2641f04386654d7f47
fbj <franz.josef.bruenner@gmail.com>
2019-04-25 21:38:00 +0200

    publish my first library, yeah!!!

--- projects/use-your-own-reg-ex/src/public-api.ts
+++ projects/use-your-own-reg-ex/src/public-api.ts
@@ -2,7 +2,4 @@
  * Public API Surface of use-your-own-reg-ex
  */
 
-/*export {RegExpHighlighter} from './pipes/regexphighlighter.pipe';
-export {RegExpSearchPipe} from './pipes/reg-exp-search.pipe';
-export {RegExTranslatorService} from './services/reg-ex-translator.service';
-*/
+export {UseYourOwnRegExModule} from './lib/use-your-own-reg-ex.module';
