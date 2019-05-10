b1e36cce91f3107a9201cb422bad5896212c0948
fbj <franz.josef.bruenner@gmail.com>
2019-05-10 19:04:20 +0200

    add ngif template experiments

--- src/app/components/shoptype/shoptype.component.ts
+++ src/app/components/shoptype/shoptype.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {Form, FormControl, FormControlName, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-shoptype',
@@ -8,10 +9,30 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class ShoptypeComponent implements OnInit {
 
+  thenBlock: TemplateRef<any>|null = null;
+
+  myForm: FormGroup = null;
+  formControlName: string = null;
+  formControl: FormControl = new FormControl();
+  myFormControl: FormControl = null;
+
+  show: boolean = true;
+  show2: boolean = true;
+
+  @ViewChild('primaryBlock')
+  primaryBlock: TemplateRef<any>|null = null;
+  @ViewChild('secondaryBlock')
+  secondaryBlock: TemplateRef<any>|null = null;
+
   constructor(private route: ActivatedRoute) { }
+
+  switchTemplate(){
+    this.thenBlock = this.thenBlock === this.primaryBlock ? this.secondaryBlock : this.primaryBlock;
+  }
 
   ngOnInit() {
     console.log('shop-type init');
+    this.thenBlock = this.primaryBlock;
     this.route.paramMap.subscribe((data)=>{
       console.log(data);
     })
