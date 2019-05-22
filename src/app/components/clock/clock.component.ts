a4d1a13029b069d7733b99a90035d97eed2ef93e
fbj <franz.josef.bruenner@gmail.com>
2019-05-22 21:35:44 +0200

    use a decorator

--- src/app/components/clock/clock.component.ts
+++ src/app/components/clock/clock.component.ts
@@ -1,10 +1,28 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
 
+function OnDestroyDecorator(decArgs: any): ClassDecorator {
+  return (target: Function) => {
+
+    const ngOnDestroy: Function = target.prototype.ngOnDestroy;
+    target.prototype.ngOnDestroy = ( ...args ) => {
+
+      console.log('ngOnDestroy:', target.name, decArgs);
+
+      if ( ngOnDestroy ) {
+        ngOnDestroy.apply(target, args);
+      }
+    };
+  };
+}
+
 @Component({
   selector: 'app-clock',
   templateUrl: './clock.component.html',
   styleUrls: ['./clock.component.scss'],
+})
+@OnDestroyDecorator({
+  arg: 'argument'
 })
 export class ClockComponent implements OnInit, OnDestroy {
 
